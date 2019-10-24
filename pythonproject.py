41d0a7bb0d9e4cb1681272d624d805eedf30144c
P3nr0se <55242595+P3nr0se@users.noreply.github.com>
2019-10-24 10:55:59 -0400

    Uploaded File
    Incomplete class and list filled with lists of celestial bodies in the solar system

---
+++ pythonproject.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+class Solar_System:
+    def class_method(system):
+        def __init__(system, bodies, star, planets, moons):
+            system.bodies = bodies
+            system.star = star
+            system.planets = planets
+            system.moons = moons
+            def bodies:
+                bodies = [Sol, Mercury, Venus, Earth, Luna, Mars, Deimos, Phobos, Ceres, Asteroid_Belt, Jupiter, Adrastea, Aitne, Amalthea, Ananke, Aoede, Arche, Autonoe, Callisto, Carme, Cllirrhoe, Carpo, Chaldene, Cyliene, Elara, Erinome, Euanthe, Eukelade, Euporie, Europa, Eurydome, Ganymede, Harpalyke, Hegemone, Helike, Hemippe, Himalia, Io, Iocaste, Isonone, Kale, Kallichore, Kalyke, Kore, Leda, Lysithea, Magaclite, Metis, Mneme, Orthosie, Pasiphae, Pasithee, Prcidike, Sinope, Sponde, Taygete, Thebe, Thelxinoe, Themisto, Thyone, Saturn, Aegir, Albiorix, Anthe, Atlas, Bebhionn, Bergelmir, Bestia, Calypso, Daphnis, Dione, Enceladus, Epimetheus, Erriapo, Farbauti, Fenrir, Fornjor, Greip, Hate, Helene, Hyperion, Hyrokkin, Iapetus, Ijiraq, Janus, Jarnsaxa, Kari, Kiviuq, Loge, Methone, Mimas, Mundilfari, Narvi, Paaliaq, Pan, Pandora, Phoebe, Polydeuces, Prometheus, Rhea, Siarnq, Skathi, Skoll, Surtur, Suttungr, Tarqeq, Tarvos, Telesto, Tethys, Thrymr, Titan, Ymir, Uranus, Ariel, Belinda, Bianca, Caliban, Cordelia, Cressida, Cupid, Desdemona, Ferdinand, Fransisco, Juliet, Mab, Margaret, Miranda, Oberon, Ophelia, Perdita, Portia, Prospero, Puck, Rosalind, Setebos, Stephano, Sycorax, Titania, Trinculo, Umbriel, Neptune, Despina, Galatea, Halimede, Larissa, Laomedeia, Naiad, Nereid, Neso, Proteus, Psamathe, Sao, Thalassa, Triton, Pluto, Charon, Nix, Hydra, Kerberos, Styx, Makemake, Eris, Dysnomia, Haumea, Namaka, Hiiaka]
+            def star:
+                star = [Sol]
+            def planets:
+                planets = [MErcury, Venus, Earth, Mars, Jupiter, ]
+    
+        
+
+
+# In[ ]:
+
+
+
+
