66c014709237d656721f36053da6b2b8082b186a
P3nr0se <55242595+P3nr0se@users.noreply.github.com>
2019-11-07 10:54:33 -0500

    Update .11.2
    attempting to add changing list based on user input and make a building system

--- main.py
+++ main.py
@@ -7,21 +7,19 @@ Start = open(IntroductoryText.txt)
 #Attempting to create a resource system for the game
 class Logistics:
     def __init__(self, fuel, materials, hrm, armour_materials, cold_resistant_materials):
-    #Fuelgauge is meant to define the resource used to get from place to place.
+        #Fueltank is meant to define the resource used to get from place to place.
         fueltank = fuel
         if fueltank < 1:
             print "We can't go anywhere! Produce more fuel or the project can no longer continue."
         if fueltank > 0:
             print fueltank
     #materials_list is meant to define the base material used to construct ships or buildings
-
         materialslog = materials
         if materialslog < 1:
             print "We can't make anything without materials! Please mine more or we won't be going anywhere."
         if materialslog > 0:
             print materialslog
     #heat_resistant materials refer to materials needed to build ships that can go to venus and mercury
-
         heat_resistant_materials = hrm
         if heat_resistant_materials < 1:
             print "We can't make these ships without heat resistant materials!"
@@ -49,3 +47,21 @@ class Logistics:
         return am
     def getcoldmats(self):
         return crm
+    def initialresources():
+        stuff = [fueltank, materialslog, heat_resistant, am, crm]
+        fueltank = 10
+        materialslog = 50
+        heat_resistant = 0
+        am = 0
+        crm = 0
+        return stuff
+    def resources(self):
+        print ["Fuel", fueltank, "Materials", materialslog, "Heat Resistant Materials", heat_resistant_materials, "Armour Materials", am, "Cold Resistant Materials", crm]
+
+class Building:
+    def __init__(self, Construct)
+    import Logistics
+        Construct = build
+        def build(rocket):
+            for rocket in build:
+                clear():
