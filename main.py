b938001f2fefd8e85edbb8d287f50ee62756ab58
P3nr0se <55242595+P3nr0se@users.noreply.github.com>
2019-11-13 18:43:30 -0500

    Update main.py

--- main.py
+++ main.py
@@ -6,37 +6,19 @@
 Start = open(IntroductoryText.txt)
 #Attempting to create a resource system for the game
 class Logistics:
+    numrockets = 0
     def __init__(self, fuel, materials, hrm, armour_materials, cold_resistant_materials):
         #Fueltank is meant to define the resource used to get from place to place.
         fueltank = fuel
-        if fueltank < 1:
-            print "We can't go anywhere! Produce more fuel or the project can no longer continue."
-        if fueltank > 0:
-            print fueltank
     #materials_list is meant to define the base material used to construct ships or buildings
         materialslog = materials
-        if materialslog < 1:
-            print "We can't make anything without materials! Please mine more or we won't be going anywhere."
-        if materialslog > 0:
-            print materialslog
     #heat_resistant materials refer to materials needed to build ships that can go to venus and mercury
         heat_resistant_materials = hrm
-        if heat_resistant_materials < 1:
-            print "We can't make these ships without heat resistant materials!"
-        if heat_resistant_materials > 0:
-            print  heat_resistant_materials
     #armour_materials are supposed to be used for ships that need to travel through the asteroid belt.
         am = armour_materials
-        if am < 1:
-            print "We can't make these ships without armour material!"
-        if am > 0:
-            print am
     #cold_resistant_materials are meant to be used for ships that travel past Jupiter
         crm = cold_resistant_materials
-        if crm < 0:
-            print "We can't make these ships without cold resistant materials!"
-        if crm > 0:
-            print crm
+    #This is meant to be the user's method of interfacing with the resources that they have access to
     def getfuel(self):
         return fueltank
     def getmats(self):
@@ -47,21 +29,33 @@ class Logistics:
         return am
     def getcoldmats(self):
         return crm
-    def initialresources():
-        stuff = [fueltank, materialslog, heat_resistant, am, crm]
-        fueltank = 10
-        materialslog = 50
-        heat_resistant = 0
-        am = 0
-        crm = 0
-        return stuff
-    def resources(self):
-        print ["Fuel", fueltank, "Materials", materialslog, "Heat Resistant Materials", heat_resistant_materials, "Armour Materials", am, "Cold Resistant Materials", crm]
-
-class Building:
-    def __init__(self, Construct)
-    import Logistics
-        Construct = build
-        def build(rocket):
-            for rocket in build:
-                clear():
+    #Used to construct a basic rocket.
+    def BuildRocket(self):
+        materialslog -= 10
+        numrockets += 1
+    #Used to Launch a Rocket to the moon
+    def EarthtoLuna(self):
+        if numrockets > 0 and fueltank >= 15:
+            numrockets -= 1
+            fueltank -= 15
+            #trying to add a condition that recognizes human presence on the moon
+            MoonLand == yes
+            open(MoonTouchdown.txt)
+        else:
+            if numrockets == 0:
+                print "We need a rocket to get to the moon!"
+            else:
+                print "We need more fuel!"
+    #Used to launch a rocket to Mars
+    def EarthtoMars(self):
+        if numrockets > 0 and fueltank >= 25:
+            numrockets -= 1
+            fueltank -= 25
+            #trying to add a condition that recognizes human presence on Mars
+            MarsLand == yes
+            open(MarsTouchdown.txt)
+        else:
+            if numrockets == 0:
+                print "We need a rocket to get to mars!"
+            else:
+                print "We need more fuel!"
