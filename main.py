2900bb435f0f7bf963c9ae8c9a5441620e2ddcdd
P3nr0se <55242595+P3nr0se@users.noreply.github.com>
2019-11-13 19:26:20 -0500

    Update .12
    Added Bases and exploitation of planet materials.  And added intiail values to resources, and initial checks to verify that the player does not start on Mars and the moon.
    
    Also edited the fluff text files slightly.
    
    This is tiring.

--- main.py
+++ main.py
@@ -6,7 +6,19 @@
 Start = open(IntroductoryText.txt)
 #Attempting to create a resource system for the game
 class Logistics:
+    # Defined initial values of resources.
     numrockets = 0
+    fueltank = 30
+    materialslog = 25
+    heat_resistant_materials + 0
+    am = 0
+    crm = 0
+    #Initial checks to tell the player that they are not on any of these planets.
+    MarsLand != yes
+    MarsBase != yes
+    MarsMine != yes
+    MoonLand != yes
+    MoonBase != yes
     def __init__(self, fuel, materials, hrm, armour_materials, cold_resistant_materials):
         #Fueltank is meant to define the resource used to get from place to place.
         fueltank = fuel
@@ -19,6 +31,8 @@ class Logistics:
     #cold_resistant_materials are meant to be used for ships that travel past Jupiter
         crm = cold_resistant_materials
     #This is meant to be the user's method of interfacing with the resources that they have access to
+    def getrockets(self):
+        return numrockets
     def getfuel(self):
         return fueltank
     def getmats(self):
@@ -29,6 +43,13 @@ class Logistics:
         return am
     def getcoldmats(self):
         return crm
+    def getrequirements(self):
+        print "The basic rocket requires 10 basic materials and the 'BuildRocket' command."
+        if MoonLand != yes:
+            print "Traveling to the moon requires fifteen fuel and the 'EarthtoLuna' command"
+        if MarsLand != yes:
+            print "Traveling to mars requires twenty-five fuel."
+        print "More resources are gained by constructing bases on the surface of the planet, which will be explained upon touchdown"
     #Used to construct a basic rocket.
     def BuildRocket(self):
         materialslog -= 10
@@ -59,3 +80,43 @@ class Logistics:
                 print "We need a rocket to get to mars!"
             else:
                 print "We need more fuel!"
+    def BuildMars(self):
+        if MarsLand != yes:
+            return "We aren't on Mars Commander!"
+        if materials_list != 10:
+            return "We don't have the Materials to Construct anything Commander!"
+        if MarsLand == yes and materials_list == 10:
+            print "We may construct a based on Mars! Yes or No Commander?"
+            if imput == yes:
+            materials_list -= 10
+            MarsBase == Yes
+                return "Type in 'MarsBase' to acess its functions Commander!"
+    def MarsBase(self):
+        if MarsBase == yes:
+            print "The Mars Base has a variety of construction options. Please type 'MarsMine' to generate materials. Otherwise please type 'MarsFuel' to generate fuel!"
+    def MarsMine(self):
+        if MarsBase != yes or MarsMine == yes:
+            return "Something's not right here! We aren't on mars or we have already mined Mars!"
+        if MarsBase == yes and MarsMine != yes:
+            am = 15
+            materials_list += 25
+            return "Orders confirmed, commander! Minerals being stockpiled"
+    def BuildMoon(self):
+        if MarsLand != yes:
+            return "We aren't on the Moon Commander!"
+        if materials_list != 10:
+            return "We don't have the Materials to Construct anything Commander!"
+        if MarsLand == yes and materials_list == 10:
+            print "We may construct a based on Moon! Yes or No Commander?"
+            if imput == yes:
+            materials_list -= 10
+            MarsBase == Yes
+                return "Type in 'MoonBase' to acess its functions Commander!"
+    def MoonBase(self):
+        if MarsBase == yes:
+            print "The Moon Base has a variety of construction options. Please type 'MoonMine' to generate materials. Otherwise please type 'MarsFuel' to generate fuel!"
+    def MoonMine(self):
+        if MoonBase != yes or MoonMine == yes:
+            return "Something's not right here! We aren't on mars or we have already mined Mars!"
+        if MoonBase == yes and MoonMine != yes:
+            print "Orders confirmed, commander! Minerals being stockpiled"
